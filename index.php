58b8cc86c8a4a7453a60b05c184ebc47da727935
kima <kima406@gmail.com>
2017-04-29 18:06:53 +0300

    added createaction

--- index.php
+++ index.php
@@ -26,5 +26,4 @@ if(isset($_POST['name']) && isset($_POST['description']) && isset($_POST['create
     $statement->bindValue(':created_at', $_POST['created_at']);
     var_dump($statement->execute());
 }
-//aaa
 
